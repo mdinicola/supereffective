e8730ebd2afc3dae55fc3832d9b7f46e56c04183
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-06-25 16:52:47 +0200

    feat: use the new HOME sprites

--- packages/website/src/styles/legacy/PkSpriteStyles.tsx
+++ packages/website/src/styles/legacy/PkSpriteStyles.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 
-const baseurl = 'https://itsjavi.com/supereffective-assets/assets/images/spritesheets/pokemon'
-const version = '20230430-0'
+const baseurl =
+  'https://itsjavi.com/supereffective-assets/assets/images/spritesheets/pokemon/home3d-icon/regular'
+const version = '20230625-0'
 
 const PkSpriteStyles = () => {
   return (
     <>
       <Head>
-        <link rel="preload" as="image" href={`${baseurl}/spritesheet.webp?v=${version}`} />
+        <link rel="preload" as="image" href={`${baseurl}/spritesheet-shadow.webp?v=${version}`} />
         <link rel="preload" as="image" href={`${baseurl}/spritesheet.webp?v=${version}`} />
         <link rel="preload" as="image" href={`${baseurl}/spritesheet-shiny.webp?v=${version}`} />
         <link rel="stylesheet" href={`${baseurl}/spritesheet.css?v=${version}`} />
