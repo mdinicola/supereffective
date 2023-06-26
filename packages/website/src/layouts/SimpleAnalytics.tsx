855c896fa354ff5abf68b2c34f0e7107b2ab7a24
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-06-26 11:25:28 +0200

    chore: stop sending analytics to axiom

--- packages/website/src/layouts/SimpleAnalytics.tsx
+++ packages/website/src/layouts/SimpleAnalytics.tsx
@@ -1,38 +1,5 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
-import { log as axiomLogger } from 'next-axiom'
-
 export function SimpleAnalytics(): JSX.Element | null {
-  const clientInfoTracked = useRef(false)
-  const eventName = 'device_info__axios'
-
-  useEffect(() => {
-    if (!clientInfoTracked.current) {
-      const orientation = window?.screen?.orientation?.type ?? 'unknown'
-      const referrerWithoutQuery = document?.referrer?.split('?')[0] ?? undefined
-
-      const eventData = {
-        screenResolution: `${window?.screen?.width}x${window?.screen?.height}`,
-        viewportWidth: document?.documentElement?.clientWidth || 'unknown',
-        viewportHeight: document?.documentElement?.clientHeight || 'unknown',
-        pixelRatio: window?.devicePixelRatio || 'unknown',
-        colorDepth: window?.screen?.colorDepth || 'unknown',
-        orientation: orientation.includes('portrait')
-          ? 'portrait'
-          : orientation.includes('landscape')
-          ? 'landscape'
-          : orientation,
-        timezone: Intl.DateTimeFormat()?.resolvedOptions()?.timeZone || 'unknown',
-        language: window?.navigator?.language || 'unknown',
-        referrerUrl: referrerWithoutQuery,
-      }
-
-      axiomLogger.info(eventName, eventData)
-
-      clientInfoTracked.current = true
-    }
-  }, [])
-
   return null
 }
