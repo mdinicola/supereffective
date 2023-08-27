0eac69d81f301318bf0b752f287c35cf100d99bb
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-08-27 02:50:09 +0200

    chore: ignore styled-system generated files

--- packages/website-beta/styled-system/jsx/fullwords.mjs
+++
@@ -1,8 +0,0 @@
-import { createElement, forwardRef } from 'react'
-import { styled } from './factory.mjs';
-import { getFullwordsStyle } from '../patterns/fullwords.mjs';
-
-export const Fullwords = /* @__PURE__ */ forwardRef(function Fullwords(props, ref) {
-  const styleProps = getFullwordsStyle()
-return createElement(styled.div, { ref, ...styleProps, ...props })
-})
--- packages/website-beta/styled-system/jsx/vscrollable.mjs
+++
@@ -1,8 +0,0 @@
-import { createElement, forwardRef } from 'react'
-import { styled } from './factory.mjs';
-import { getVscrollableStyle } from '../patterns/vscrollable.mjs';
-
-export const Vscrollable = /* @__PURE__ */ forwardRef(function Vscrollable(props, ref) {
-  const styleProps = getVscrollableStyle()
-return createElement(styled.div, { ref, ...styleProps, ...props })
-})
