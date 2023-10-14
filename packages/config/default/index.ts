ba9526c242dd765a1cefbb12b8ace6a842cc829b
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-10-14 15:29:58 +0200

    fix: change ppal donate url

--- packages/config/default/index.ts
+++ packages/config/default/index.ts
@@ -1,5 +1,5 @@
-import { isDevelopmentEnv } from '@pkg/utils/lib/env'
-import { getBaseUrl } from '@pkg/utils/lib/nextjs/urls'
+import {isDevelopmentEnv} from '@pkg/utils/lib/env'
+import {getBaseUrl} from '@pkg/utils/lib/nextjs/urls'
 
 // !!! Changing these versions will make Vercel to optimize all images again
 //      (producing costs of 5$ every 1000 extra images)
@@ -30,7 +30,7 @@ const config = {
     issue_report: 'https://github.com/itsjavi/supereffective/issues',
     roadmap: 'https://github.com/users/itsjavi/projects/9',
     discord: 'https://discord.gg/3fRXQFtrkN',
-    paypal_donate: 'https://www.paypal.me/metaunicorn/10',
+    paypal_donate: 'https://www.paypal.me/itsjavidotcom/10',
     legacy_account_recovery_form: 'https://forms.gle/HxnV3qCs1UWJn7Tc6',
   },
   patreon: {
