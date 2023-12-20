bc2400397732071e7bca1951d167c40149cea0d3
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-12-20 17:48:43 +0100

    fix: production env vars conflicts

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@ const path = require('node:path')
 const { withAxiom } = require('next-axiom')
 
 if (!process.env.VERCEL && !process.env.VERCEL_URL) {
-  process.env.VERCEL_URL = 'http://localhost:' + (process.env.PORT || 3000)
+  process.env.VERCEL_URL = 'localhost:' + (process.env.PORT || 3000)
 }
 
-if (!process.env.NEXTAUTH_URL) {
-  process.env.NEXTAUTH_URL = process.env.VERCEL_URL
+if (!process.env.VERCEL && !process.env.NEXTAUTH_URL) {
+  const protocol = process.env.VERCEL_URL.includes('localhost') ? 'http://' : 'https://'
+  process.env.NEXTAUTH_URL = protocol + process.env.VERCEL_URL
 }
 
 /** @type {import('next').NextConfig} */
