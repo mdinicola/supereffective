21aad2457fcdb83b8588e31c2ea255cac15a4e1c
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-12-20 06:52:34 +0100

    fix style paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import AppLayout from '@/lib/components/layout/AppLayout'
 
 //1;
-import '@supeffective/icons/dist/index.css'
+import '@supeffective/icons/styles.css'
 //2;
 import '@/styles/styles.css'
 
