d76c978e1b3e6334407725bf2c37ac768965b2ec
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-03-18 05:27:08 +0100

    refactor: get rid of tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import AppLayout from '@/lib/components/layout/AppLayout'
 
-//1; Tailwind CSS (with reset / preflight)
-import '@/styles/tailwind/styles.css'
-//2; SuperEffective Icons
+//
+// 1 - normalize
+import '@/styles/normalize.css'
+//
+
+//
+// 2 - base
+import '@/styles/base.css'
+//
+
+//
+// 3 - SuperEffective Icons
 import '@supeffective/icons/styles.css'
-//3; Legacy CSS (needs cleanup and refactoring)
+//
+
+//
+// 4 - Legacy CSS (needs cleanup and refactoring)
 import '@/styles/legacy/styles.css'
+
+//
 
 export default AppLayout
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    // 'postcss-import': {}, // needed to organize @tailwindcss imports in nested files
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
